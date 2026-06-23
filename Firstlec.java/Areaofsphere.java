41a40d7dd73b394d8a215653cc56f2d468706357
vineetsingh3806-eng <vineetsingh3806@gmail.com>
2026-06-23 23:15:14 +0530

    added java programs

---
+++ Firstlec.java/Areaofsphere.java
@@ -0,0 +1,6 @@
+public class Areaofsphere {
+    public static void main(String[] args) {
+        double r = 2.3;
+        System.out.println(4*3.14*r*r);
+    }
+}
---
+++ javacodes.java
@@ -0,0 +1,6 @@
+public class Areaofcircle {
+    public static void main(String[] args) {
+        double r = 5;
+    System.out.println(3.14*r*r);
+ }
+}
