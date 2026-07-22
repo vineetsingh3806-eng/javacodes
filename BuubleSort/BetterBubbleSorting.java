f2963211ccf595fc32b830c9f121874bd0dd3c55
Vineet singh <vineetsingh3806@gmail.com>
2026-07-22 21:44:21 +0530

    noooooooo

--- BuubleSort/BetterBubbleSorting.java
+++ BuubleSort/BetterBubbleSorting.java
@@ -1,6 +1,6 @@
 public class BetterBubbleSorting {
     public static void main(String[]args){
-        int[]arr={5,-2,6,7,2,0,7,2};
+        int[]arr={5,-2,6,2,2,0,7,7};
         int n=arr.length;
          for(int i=0;i<n-1;i++){
             // check after every pass that array is sorted or not..
@@ -8,21 +8,23 @@ public class BetterBubbleSorting {
             //bubble sort starts from here...
             for(int j=0;j<n-1-i;j++){
                 if(arr[j]>=arr[j+1]){
-                     int temp=arr[j];
+                    int temp=arr[j];
                     arr[j]=arr[j+1];
-                    arr[j+1]= temp;   //bubble sort  starts from here..
+                    arr[j+1]= temp;  
                     flag=false;    //aray is not sorted....
-                }          
+                   
+                }
             }
             if(flag==true){
                 System.out.println("array is sorted");
                 break;
-            } 
+            }
         }
-       for(int ele:arr){
+        //print the sorted array...
+         for(int ele:arr){
         System.out.print(ele + " ");
-       }
-    }
+         }
+}
 }
 
 
